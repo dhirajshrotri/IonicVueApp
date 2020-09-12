f9ab4a717de3eb9b49bcdfa88baab72a1506554e
dhiraj <dhiraj.shrotri@gmail.com>
2020-09-12 22:46:24 +0530

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,34 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import Ionic from '@ionic/vue';
+import '@ionic/core/css/ionic.bundle.css';
+import '@/assets/styles/index.css';
 
 Vue.config.productionTip = false
+Vue.use(Ionic);
+
+import { IonicVueRouter } from "@ionic/vue";
+Vue.use(IonicVueRouter);
+
+const router = new IonicVueRouter({
+  routes: [
+    { path: "/", redirect: "/home" },
+    {
+      path: "/home",
+      name: "home",
+      component: () =>
+        import(/* webpackChunkName: "home" */ "@/components/HomePage"),
+    },
+    {
+      path: "/new-item",
+      name: "new-item",
+      component: () => 
+        import(/* webpackChunkName: "new-item" */ "@/components/NewItemPage"),
+    }
+  ]
+});
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
